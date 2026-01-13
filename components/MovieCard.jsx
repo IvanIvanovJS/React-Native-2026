0cb9102f1486dcf79f73fe02e33ef07d7e17ca1f
IvanIvanovJS <sibsmartco@gmail.com>
2026-01-13 12:58:28 +0200

    feat: enhance MovieCard layout with button and improved styling

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -1,19 +1,22 @@
-import { Image, Text, View } from "react-native";
+import { Button, Image, Text, View } from "react-native";
 
 export default function MovieCard({ id, title, year, imageUrl }) {
 	return (
-		<View>
-			<View>
-				<Text>{title}</Text>
-				<Text>{year}</Text>
-			</View>
+		<View style={{ flexDirection: "row", gap: "10", marginTop: 20 }}>
 			<Image
 				source={{
 					uri: imageUrl,
 					width: 184,
-					height: 250,
+					height: 280,
 				}}
 			></Image>
+			<View style={{ gap: 10 }}>
+				<Text>{title}</Text>
+				<Text>{year}</Text>
+				<View style={{ width: 100 }}>
+					<Button title="Details"></Button>
+				</View>
+			</View>
 		</View>
 	);
 }
